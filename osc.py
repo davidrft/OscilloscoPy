66f838653432b17b1294aa479ae609b674d1606d
David Riff <davidriff@outlook.com>
2017-05-23 15:14:59 -0300

    Implemented functions: 	reset 	get_time_scale 	get_volt_scale 	get_volt_offset
    Function get_time_offset was implemented but is getting a time out

--- osc.py
+++ osc.py
@@ -8,16 +8,37 @@ def list_devices():
             inst = usbtmc.Instrument(i)
             print(inst.ask("*IDN?"))
 
-class Oscilloscope(usbtmc.Instrument):
+class Osc(usbtmc.Instrument):
     
     inst = None
 
     def __init__(self, pos):
-        self.__super__(usbtmc.list_resources()[pos])
+        super(Osc, self).__init__(usbtmc.list_resources()[pos])
+        
+        
+    def reset(self):
+        self.write("*RST")
+        
+        
+    def get_time_scale(self):
+        return float(self.ask(":TIM:SCAL?"))
+        
+        
+    def get_time_offset(self):
+        return float(self.ask(":TIM:OFFS?"))
+        
+        
+    def get_volt_scale(self, channel):
+        return float(self.ask(":CHAN" + str(channel) + ":SCAL?"))
+    
+    
+    def get_volt_offset(self, channel):
+        return float(self.ask(":CHAN" + str(channel) + ":OFFS?"))
 
-
+    #def setScale(x):
+        
 def main():
-    list_devices()
+    x = osc.Osc(0)
 
 if __name__ == "__main__":
     main()
